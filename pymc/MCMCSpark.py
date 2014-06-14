59140da51f2a2d97d205416cffd8c082aee2bc88
Mert Terzihan <mertterzihan@gmail.com>
2014-06-13 17:04:46 -0700

    Adapt MCMC methods to MCMCSpark

--- pymc/MCMCSpark.py
+++ pymc/MCMCSpark.py
@@ -8,20 +8,25 @@ For an example usage, please look at pymc/examples/disaster_model_spark.py
 
 __all__ = ['MCMCSpark']
 
-from .MCMCMultiChain import MCMCMultiChain
-from pymc.database import ram
+from .MCMC import MCMC
+from pymc.database import spark
+
 
 import numpy as np 
 import os
 from pyspark import SparkContext
 from pymc import six
+import copy
+print_ = six.print_
+
+Supported_Backends = ['spark', 'hdfs']
 
 class MCMCSpark():
 	'''
 	For each Spark job, an MCMC instance is created
 	status.txt belongs to the last job
 	'''
-	def __init__(self, input=None, db='hdfs', name='MCMC', calc_deviance=True, nJobs=1, **kwargs):
+	def __init__(self, input=None, db='spark', name='MCMC', calc_deviance=True, nJobs=1, **kwargs):
 		'''
 		Parameters
 		----------
@@ -31,7 +36,7 @@ class MCMCSpark():
 			The name of the database backend that will store the values
 			of the stochastics and deterministics sampled during the MCMC loop.
 		- nJobs : integer
-			Number of threads that will run MCMC
+			Number of Spark jobs that will run MCMC
 		- **kwarg : dict
 			- spark_host : str
 				Cluster URL to connect to
@@ -52,22 +57,23 @@ class MCMCSpark():
 		'''
 		if nJobs < 1:
 			nJobs = 1
-		self.spark_host = kwargs.pop("spark_host", None)
-		self.spark_home = kwargs.pop("spark_home", None)
-		self.model_file = kwargs.pop("model_file", None)
-		if not all([self.spark_host, self.spark_home, self.model_file]):
-			raise ValueError('Spark Host and Spark Home cannot be None!')
-		self.obs_files = kwargs.pop("obs_files", None)
+		self._check_database_backend(db)
+		if self.save_to_hdfs:
+			self.dbname = kwargs.pop("dbname", None)
+			if self.dbname is None:
+				raise ValueError('Please provide a directory on HDFS to save files')
+		self.sc = kwargs.pop("spark_context", None)
+		if self.sc is None:
+			raise ValueError('Please provide SparkContext')
+		# self.model_file = kwargs.pop("model_file", None)
+		self.input = input
+		# if self.model_file is None and self.input is None:
+			# raise ValueError('Please provide a model')
 		self.nJobs = nJobs
-		self.db = db
 		self.name = name
 		self.calc_deviance = calc_deviance
+		# self.sc.addPyFile(self.model_file)
 		self.kwargs = kwargs
-		self.sc = SparkContext(master=self.spark_host, appName=self.name+self.db, sparkHome=self.spark_home)
-		if self.obs_files is not None:
-			for f in self.obs_files:
-				self.sc.addFile(f)
-		self.sc.addPyFile(self.model_file)
 
 	def sample(
 		self, iter, burn=0, thin=1, tune_interval=1000, tune_throughout=True,
@@ -78,63 +84,186 @@ class MCMCSpark():
 		Similar to MCMC.sample() in terms of parameters
 		Returns a RAM database to the user
 		'''
-		db = self.db
 		name = self.name
 		calc_deviance = self.calc_deviance
 		kwargs = self.kwargs
-		model_file = self.model_file
+		#model_file = self.model_file
 		total_jobs = self.nJobs
+		input_model = self.input
 
 		def sample_on_spark(nJob):
-			model_module = os.path.splitext(os.path.basename(model_file))[0]
-			imported_module = __import__(model_module)
+			# model_module = os.path.splitext(os.path.basename(model_file))[0]
+			# imported_module = __import__(model_module)
 			final_job = False
 			if total_jobs-1 == nJob:
 				final_job = True
-			m = MCMCMultiChain(imported_module, db=db, name=name, calc_deviance=calc_deviance, chainNum=nJob, final_job=final_job, **kwargs)
+			m = MCMC(input_model, db='ram', name=name, calc_deviance=calc_deviance, **kwargs)
 			m.sample(iter, burn, thin, tune_interval, tune_throughout,
         		save_interval, burn_till_tuned, stop_tuning_after,
             	verbose, progress_bar)
 			container = {}
 			for tname in m.db._traces:
-				container[tname] = m.db._traces[tname]._trace
+				container[tname] = m.db._traces[tname]._trace[0]
 			container['_state_'] = m.get_state()
-			return container
+			return (nJob, container)
 
-		container = self.sc.parallelize(xrange(self.nJobs)).map(sample_on_spark).collect()
-		data = {}
-		for chain, traces in enumerate(container):
-			for name in traces.keys():
-				if chain == 0:
-					data[name] = {}
-				if name != '_state_':
-					data[name][chain] = traces[name][0]
+		rdd = self.sc.parallelize(xrange(self.nJobs)).map(sample_on_spark).cache()
+		vars_to_tally = copy.copy(rdd.map(lambda x: x[1].keys())).first()
+		vars_to_tally.remove('_state_')
+		self._variables_to_tally = set(vars_to_tally)
+		self._assign_database_backend(rdd, vars_to_tally)
+		#if self.save_to_hdfs:
+			#rdd.map().saveAsTextFile()
 
-		db = ram.Database('MCMCSparkRamDatabase')
-		db.chains = self.nJobs
-		for name, values in six.iteritems(data):
-			db._traces[name] = ram.Trace(name=name, value=values, db=db)
-			setattr(db, name, db._traces[name])
-		db._state_ = container[self.nJobs-1]['_state_']
+	def _check_database_backend(self, db):
+		'''
+		Check that given database is compatible with spark
+		'''
+		if not isinstance(db, str):
+			raise ValueError('MCMCSpark supports just strings as parameter for database backend for now')
+		elif db not in Supported_Backends:
+			raise ValueError('MCMCSpark supports currently Spark (db=\'spark\') and HDFS (db=\'hdfs\') as database backends')
+		self.save_to_hdfs = False
+		if db == 'hdfs':
+			self.save_to_hdfs = True
 
-		return db
+	def _assign_database_backend(self, db, vars_to_tally):
+		'''
+		Assign Spark RDD database
+		'''
+		self.db = spark.Database(db, vars_to_tally)
 
+	def trace(self, name, chain=-1):
+		'''
+		Return the trace of a tallyable object stored in RDD
 
-def load_data(file_name, shape=None, delimiter=','):
-	'''
-	If any observation file has been provided in __init__, then they can be loaded by this method
+		Parameters
+		----------
+		name : str
+			The name of the tallyable object
+		chain : int
+			The trace index. Setting 'chain=i' will return the trace created by the ith spark job
+		'''
+		if isinstance(name, str):
+			return self.db.trace(name, chain)
+		elif isinstance(name, Variable):
+			return self.db.trace(name.__name__, chain)
+		else:
+			raise ValueError(
+				'Name argument must be string or Variable, got %s.' %
+				name)
 
-	Parameters
-	__________
-	- file_name : str
-		Name of the file to be loaded. It needs to reside on the Spark cluster
-	- shape : list
-		Shape of the matrix that is located in the file to be loaded
-	-delimiter : str
-		Character that splits elements in the file
-	'''
-	arr = np.loadtxt(file_name, delimiter=delimiter)
-	if shape is not None:
-		arr = arr.reshape(shape)
-	return arr
-	
+	def get_state(self):
+		'''
+		Return the sampler's current state
+		'''
+		return self.db.getstate()
+
+	def summary(self, variables=None, alpha=0.05, start=0, batches=100,
+				chain=None, roundto=3):
+		'''
+		Generate a pretty-printed summary of the node.
+
+		Parameters
+		----------
+		variables : list of str
+			List of variable names to be summarized. By default it summarizes every variable
+		alpha : float
+			The alpha level for generating posterior intervals. Defaults to 0.05
+		start : int
+			The starting index from which to summarize chain. Defaults to zero
+		batches : int 
+			Batch size for calculating standard deviation for non-independent samples.
+			Defaults to 100
+		chain : int
+			The index for which chain to summarize. Defaults to None (all chains)
+		roundto : int
+			The number of digits to round posterior statistics.
+		'''
+		
+		if variables is None:
+			variables = self._variables_to_tally
+		else:
+			variables = [v for v in variables if v in self._variables_to_tally]
+		for variable in variables:
+			statdict = self.db._traces[variable].stats(alpha=alpha, start=start, 
+													   batches=batches, chain=chain)
+			self.summary_helper(variable, statdict, roundto)
+
+	def summary_helper(self, variable, statdict, roundto=3):
+		'''
+		Helper function for summary to extract summaries
+		'''
+		size = np.size(statdict['mean'])
+		print_('\n%s:' % variable)
+		print_(' ')
+		buffer = []
+		iindex = [key.split()[-1] for key in statdict.keys()].index('interval')
+		interval = list(statdict.keys())[iindex]
+		buffer += [
+			'Mean             SD               MC Error        %s' %
+			interval]
+		buffer += ['-' * len(buffer[-1])]
+		indices = range(size)
+		if len(indices) == 1:
+			indices = [None]
+		for index in indices:
+			m = str(round(statdict['mean'][index], roundto))
+			sd = str(round(statdict['standard deviation'][index], roundto))
+			mce = str(round(statdict['mc error'][index], roundto))
+			hpd = str(statdict[interval][index].squeeze().round(roundto))
+			valstr = m
+			valstr += ' ' * (17 - len(m)) + sd
+			valstr += ' ' * (17 - len(sd)) + mce
+			valstr += ' ' * (len(buffer[-1]) - len(valstr) - len(hpd)) + hpd
+			buffer += [valstr]
+		buffer += [''] * 2
+		buffer += ['Posterior quantiles:', '']
+		buffer += [
+			'2.5             25              50              75             97.5']
+		buffer += [
+			' |---------------|===============|===============|---------------|']
+		for index in indices:
+			quantile_str = ''
+			for i, q in enumerate((2.5, 25, 50, 75, 97.5)):
+				qstr = str(round(statdict['quantiles'][q][index], roundto))
+				quantile_str += qstr + ' ' * (17 - i - len(qstr))
+			buffer += [quantile_str.strip()]
+		buffer += ['']
+		print_('\t' + '\n\t'.join(buffer))
+
+	def stats(self, variables=None, alpha=0.05, start=0,
+			  batches=100, chain=None, quantiles=(2.5, 25, 50, 75, 97.5)):
+		'''
+		Statistical output for variables
+
+		Parameters
+		----------
+		variables : list of str
+			List of variable names to be summarized. By default it summarizes every variable
+		alpha : float
+			The alpha level for generating posterior intervals. Defaults to 0.05
+		start : int
+			The starting index from which to summarize chain. Defaults to zero
+		batches : int 
+			Batch size for calculating standard deviation for non-independent samples.
+			Defaults to 100
+		chain : int
+			The index for which chain to summarize. Defaults to None (all chains)
+		quantiles : tuple or list
+			The desired quantiles to be calculated. Defaults to (2.5, 25, 50, 75, 97.5)
+		'''
+		if variables is None:
+			variables = self._variables_to_tally
+		else:
+			variables = [v for v in self.variables if v in variables]
+
+		stat_dict = {}
+
+		for variable in variables:
+			stat_dict[variable] = self.trace(variable).stats(alpha=alpha, start=start,
+															 batches=batches, chain=chain, quantiles=quantiles)
+		return stat_dict
+
+	def remember(self, chain=-1, trace_index=None):
+		pass
--- pymc/examples/disaster_model_spark.py
+++ pymc/examples/disaster_model_spark.py
@@ -1,26 +1,10 @@
 from pymc.MCMCSpark import MCMCSpark
+from pymc.examples import disaster_model
 
-if __name__ == "__main__":
+input = disaster_model
+nJobs = 10
+m = MCMCSpark(input=input, nJobs=10, spark_context=sc)
+m.sample(50)
 
-	db = 'hdfs'
-	dbname = 'user/test'
-
-	# Location of the file that defines model
-	model_file = 'pymc/examples/disaster_model.py'
-
-	# HDFS Config
-	hdfs_host = 'localhost'
-	port = '50070'
-	user_name = 'test'
-
-	# Spark Config
-	master = 'local[4]'
-	spark_home = 'spark'
-
-	# Number of times the MCMC will be run on Spark
-	nJobs=10
-
-	m = MCMCSpark(db=db, dbname=dbname, model_file=model_file, spark_home=spark_home, spark_host=master, nJobs=nJobs, hdfs_host=hdfs_host, port=port, user_name=user_name)
-	db = m.sample(10)
-	print db.trace('early_mean')[:]
-	print db.trace('early_mean', chain=0)[:]
+m.summary(batches=10)
+m.db._traces['early_mean'].stats(batches=10, chain=0)
