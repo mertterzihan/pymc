7ec7bf6835aabcc96c369e62163a5b4fa4988d4f
Mert Terzihan <mertterzihan@gmail.com>
2014-07-24 17:09:32 -0700

    Added comments

--- pymc/examples/lda/HierchicalDistributedLDA.py
+++ pymc/examples/lda/HierchicalDistributedLDA.py
@@ -2,18 +2,18 @@
 Implementation of HD-LDA model that has been described in Distributed Algorithms 
 for Topic Models by D. Newman, et al. (2009).
 '''
+from random import random
+total_topics = 10
+# Seeds for global parameters, phi and beta, to enable synchronization between the jobs
+phi_seeds = [int(random()*1000000) for k in xrange(total_topics)]
+beta_seeds = [int(random()*1000000) for k in xrange(total_topics)]
 
 def model_function(data, global_param):
-	from pymc import Dirichlet, Categorical, Stochastic, Gamma, Lambda, Container, Model
+	from pymc import Dirichlet, Categorical, Gamma, Lambda, Container, Model
 	import numpy as np
-	from scipy.stats import gamma
-	from pymc.distributions import dirichlet_like
-	import math
 
 	total_partitions = 4
 	total_words = 7491
-	total_topics = 10
-	local_iter = 10
 	total_vocab = 78
 
 	a = float((total_partitions-1)*total_words)/(total_partitions*total_topics)
@@ -24,11 +24,13 @@ def model_function(data, global_param):
 
 	gamma_vector = [gamma_prior for v in xrange(total_vocab)]
 
+	# Topic dependent strength parameter
 	beta = Container([Gamma(name='beta_%i' % k, 
 							alpha=a, 
 							beta=1/b,
 							value=a*b) for k in xrange(total_topics)])
 
+	# Global word topic-distribution
 	global_phi = Container([Dirichlet('global_phi_%i' % k, 
 									 theta=gamma_vector,
 									 value=(gamma_vector/np.sum(gamma_vector))[:-1]) for k in xrange(total_topics)])
@@ -37,10 +39,12 @@ def model_function(data, global_param):
 						lambda global_phi=global_phi, beta=beta: np.multiply(np.append(global_phi[k], 1.0-np.sum(global_phi[k])), beta[k]), 
 						trace=False) for k in xrange(total_topics)]
 
+	# Local word-topic distribution
 	local_phi = Container([Dirichlet('local_phi_%i' % k, 
 									 theta=phi_param[k], 
 									 value=[1.0/total_vocab]*(total_vocab-1)) for k in xrange(total_topics)])
 
+	# Local prior parameter for document-topic distribution
 	alpha = Gamma(name='alpha', alpha=c, beta=1/d, value=c*d)
 
 	alpha_vector = Lambda('alpha_vector', 
@@ -57,10 +61,13 @@ def model_function(data, global_param):
 
 	tmp_vector = [c*d for i in xrange(total_topics)]
 	theta_value = tmp_vector/np.sum(tmp_vector)
+
+	# Document-topic distribution
 	theta = Container([Dirichlet('theta_%i' % local_docs[i][0], 
 								 theta=alpha_vector,
 								 value=theta_value[:-1]) for i in xrange(len(local_docs))])
 
+	# Topic assignments for each word
 	z = Container([Categorical('z_' + str(doc[0]), 
 							   p=theta[n], 
 							   size=len(doc[1]), 
@@ -70,6 +77,7 @@ def model_function(data, global_param):
 							   lambda local_phi=local_phi: np.append(local_phi[k], 1.0-np.sum(local_phi[k])),
 							   trace=False) for k in xrange(total_topics)]
 
+	# Modeling the observations
 	x = Container([Categorical('x_' + str(doc[0]), 
 							   p=Lambda('phi_z_%i' % doc[0], 
 							   			lambda z=z, local_phi=local_phi_lambda: [local_phi[z[n][order]]/np.sum(local_phi[z[n][order]]) for order,word in enumerate(doc[1])]), 
@@ -82,9 +90,12 @@ def step_function(mcmc):
 	import pymc as pm 
 	import numpy as np
 	from numpy.random import normal as rnormal
-	from pymc.utils import round_array
+	# from pymc.utils import round_array
 
 	class CustomMetropolis(pm.Metropolis):
+		'''
+		Custom step method based on Metropolis, that enables to propose same values to global variables
+		'''
 		def __init__(self, stochastic, seed, scale=1., proposal_sd=None,
 					 proposal_distribution=None, positive=False, verbose=-1, tally=True):
 
@@ -173,15 +184,13 @@ def step_function(mcmc):
 				# Round before setting proposed value
 				self.stochastic.value = new_val
 
-	K = 10
-	phi_seeds = [63194, 65349, 72134, 35345, 175, 63244, 10852, 32108, 88629, 47487]
-	beta_seeds = [30256, 29172, 9111, 24805, 40423, 25123, 79462, 95160, 7287, 84682]
 	import re
 	# Apply this custom step method to all topic assignment variables, namely z's
 	pattern = re.compile('z_')
 	params = [p for p in mcmc.variables if pattern.match(p.__name__)]
 	for z in params:
-		mcmc.use_step_method(HybridRandomWalk, z, 0, K)
+		mcmc.use_step_method(HybridRandomWalk, z, 0, total_topics)
+	# Global parameters use the CustomMetropolis step method
 	pattern = re.compile('global_phi_')
 	params = [p for p in mcmc.variables if pattern.match(p.__name__)]
 	for n, p in enumerate(params):
@@ -195,7 +204,7 @@ from pymc.DistributedMCMC import DistributedMCMC
 
 # The path of the txt file that was produced by the preprocess_nips.py script
 path = 'hdfs:///user/test/data/nips.txt'
-#global_update=('phi', global_update)
+
 m = DistributedMCMC(spark_context=sc, 
 					model_function=model_function, 
 					nJobs=4, 
